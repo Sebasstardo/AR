7194e1e28b636fc43de5646540f764e8614beac7
= <65838126+Sebasstardo@users.noreply.github.com>
2022-07-24 14:38:05 -0400

    primeraParte

--- js/main.js
+++ js/main.js
@@ -1,3 +1,18 @@
 $(document).ready(function () {
   new WOW().init();
 });
+
+jQuery(window).scroll(function () {
+  if ($("#menu").offset().top > 790) {
+    $("#menu").addClass("fijo");
+  } else {
+    $("#menu").removeClass("fijo");
+  }
+});
+
+jQuery("ul.navbar-nav > li").click(function (e) {
+  console.log("first");
+  e.preventDefault();
+  $("ul.navbar-nav > li").removeClass("active");
+  $(this).addClass("active");
+});
