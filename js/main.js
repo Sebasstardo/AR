41a69c1f4114c40ac1afca1357f90714203e93c6
= <65838126+Sebasstardo@users.noreply.github.com>
2022-07-31 13:34:53 -0400

    update 2

--- js/main.js
+++ js/main.js
@@ -1,5 +1,24 @@
 $(document).ready(function () {
   new WOW().init();
+
+  jQuery("#btnVerMas").click(function () {
+    $("#parrCorp").toggleClass("hidden shown");
+  });
+  jQuery("#btnVerMasLi").click(function () {
+    $("#parrCorpLi").toggleClass("hidden shown");
+  });
+  jQuery("#btnVerMasLab").click(function () {
+    $("#parrLab").toggleClass("hidden shown");
+  });
+  jQuery("#btnVerHow").click(function () {
+    $("#parrhow").toggleClass("hiddenHow shownHow");
+  });
+  jQuery("#btnVerHow2").click(function () {
+    $("#parrhow2").toggleClass("hiddenHow shownHow");
+  });
+  jQuery("#btnVerHow3").click(function () {
+    $("#parrhow3").toggleClass("hiddenHow shownHow");
+  });
 });
 
 jQuery(window).scroll(function () {
@@ -8,11 +27,4 @@ jQuery(window).scroll(function () {
   } else {
     $("#menu").removeClass("fijo");
   }
-});
-
-jQuery("ul.navbar-nav > li").click(function (e) {
-  console.log("first");
-  e.preventDefault();
-  $("ul.navbar-nav > li").removeClass("active");
-  $(this).addClass("active");
 });
